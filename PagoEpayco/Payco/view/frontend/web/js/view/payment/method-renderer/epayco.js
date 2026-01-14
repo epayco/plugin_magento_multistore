dccdc4bd60f06fbc9a2dbc4c2970354633d8cba8
RicardoSaldarriagapayco <ricardo.saldarriaga@epayco.com>
2026-01-14 16:10:39 -0500

    feat:Se actualiza version de checkout 2

--- PagoEpayco/Payco/view/frontend/web/js/view/payment/method-renderer/epayco.js
+++ PagoEpayco/Payco/view/frontend/web/js/view/payment/method-renderer/epayco.js
@@ -46,7 +46,7 @@ define(
                             localStorage.setItem("epayco_quote_id", getQuoteId);
                             var data = localStorage.getItem("epayco_invoice");
                             if (data) {
-                                _this.onEpaycoSuccess(data, _this);
+                                _this.onEpaycoSuccess(data, _this, getQuoteId);
                             } else {
                                 fullScreenLoader.stopLoader();
                                 alert({
@@ -64,7 +64,7 @@ define(
                                 async: false,
                                 data:  { "order_id": getQuoteId },
                                 success: function(data) {
-                                    _this.onEpaycoSuccess(data, _this);
+                                    _this.onEpaycoSuccess(data, _this, getQuoteId);
                                 },
                                 error: function(error) {
                                     fullScreenLoader.stopLoader();
@@ -89,7 +89,7 @@ define(
                     console.log('error: ' + error);
                 }
             },
-            onEpaycoSuccess: function(data, _this){
+            onEpaycoSuccess: function(data, _this, getQuoteId){
                 if(data.success){
                     var ip = this.getCustomerIp();
                     var checkoutConfig= window.checkoutConfig;
@@ -176,9 +176,8 @@ define(
                         test: test,
                         checkout_version:"2",
                         extrasEpayco:{
-                            extra5:"P28"
-                        },
-                        checkout_version:2
+                            extra5:"P27"
+                        }
                     };
                     //console.log("data",data)
                     const apiKey = window.checkoutConfig.payment.epayco.payco_public_key.trim();
